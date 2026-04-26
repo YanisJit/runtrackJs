21309aa5ac131bb10fc4f1e2caf5ddf63a14604b
YanisJit <yanis.jit@laplateforme.io>
2026-04-26 18:48:51 +0200

    Terminé Jour 2 -Job01

---
+++ jour02/job01/script.js
@@ -0,0 +1,7 @@
+function citation() {
+    // On récupère le texte contenu dans l'élément qui a l'id "citation"
+    let texte = document.getElementById("citation").textContent;
+    
+    // On l'affiche dans la console
+    console.log(texte);
+}
